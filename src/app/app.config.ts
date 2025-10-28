685dbaf9ed299ca577c8b8a54b8cd8390e0aee1d
Sergey Malyuga <sergey@malyuga.ru>
2025-10-28 13:26:49 +0300

    Создать auth.interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideStore} from '@ngrx/store';
-import {provideHttpClient, withInterceptorsFromDi,} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi,} from '@angular/common/http';
 import {provideEffects} from '@ngrx/effects';
 import {appReducer} from './store/app/app.reducer';
 import {OfferEffects} from './store/offer/effects/offer.effects';
+import {AuthInterceptor} from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideStore(appReducer),
     provideHttpClient(withInterceptorsFromDi()),
     provideEffects(OfferEffects),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    }
   ],
 };
