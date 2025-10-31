26fbaf3775ab3e1de87c8c02b84cc9c7c5715b4f
Sergey Malyuga <sergey@malyuga.ru>
2025-10-31 13:39:21 +0300

    Создать favorite.service

---
+++ src/app/core/services/favorite.service.ts
@@ -0,0 +1,20 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Offer, OfferPreview} from '../models/offers';
+import {APIRoute, BASE_URL} from '../constants/const';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoriteService {
+  private http: HttpClient = inject(HttpClient);
+
+  public getFavorites(): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${APIRoute.FAVORITE}`)
+  }
+
+  public changeStatus(offerId: string, status: number): Observable<Offer> {
+    return this.http.post<Offer>(`${BASE_URL}/${APIRoute.LOGIN}/${offerId}/${status}`, {})
+  }
+}
